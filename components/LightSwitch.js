2eb088e30fcdfa79d667fbdea0a624f930c506f8
abelmuro <abelmuro93@gmail.com>
2023-06-15 19:34:46 -0700

    commit

--- components/LightSwitch.js
+++ components/LightSwitch.js
@@ -2,7 +2,6 @@ import {useEffect, useContext} from 'react';
 import {ThemeContext} from '../pages/_app';
 import styles from '../styles/LightSwitch.module.css';
 
-//now i need to change the icon image everytime theme is changed
 
 export default function LightSwitch() {
     const {theme, setTheme} = useContext(ThemeContext);
@@ -12,16 +11,18 @@ export default function LightSwitch() {
         setTheme(!theme);
     }
 
-    useEffect(() => {
-
-    }, [])
 
     return(
         <section className={styles.container}>
             <h1 className={styles.currentTheme}>
                 {theme ? 'LIGHT' : 'DARK'}
             </h1>
-            <div className={styles.currentThemeIcon} onClick={handleClick}></div>
+            <div className={theme ? 
+                [styles.currentThemeIcon, styles.light].join(' ') : 
+                [styles.currentThemeIcon, styles.dark].join(' ')} 
+                onClick={handleClick}>
+
+            </div>
         </section>
     )
 }
