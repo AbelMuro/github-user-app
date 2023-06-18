f14f24344335faeaca0e030b1360759fad3fdacf
abelmuro <abelmuro93@gmail.com>
2023-06-18 12:34:26 -0700

    commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import {memo} from 'react';
 import styles from '../styles/Layout.module.css';
 
-//this should be a different comment
+
 function Layout({children}) {
     return(
         <main className={styles.container}>
